bcdb6c25d01287cce77b6db8fbdf753313277492
_kdl_ <158837916+Kidallov@users.noreply.github.com>
2025-09-22 12:17:33 +0300

    Update and rename test_client.py to test_api.py

--- Term_1/LR_3/tests/test_client.py
+++
@@ -1,8 +0,0 @@
-from fakeweather import FakeWeatherClient
-
-def test_weather():
-    client = FakeWeatherClient(seed=1)
-    data = client.get_current_weather("London")
-    assert "name" in data
-    assert "main" in data
-    assert "weather" in data
